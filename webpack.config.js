9acd644402033f6559eb92d78d1174cbc2d4008f
Igor <igor06brw@gmail.com>
2019-10-03 22:15:48 +0200

    add rule of mp4 for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,68 +4,80 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const  { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        publicPath: './',
-        path: path.resolve(__dirname, 'dist/'),
-        filename: 'js/app.bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        writeToDisk: true,
-        watchContentBase: true,
-        port: 9876
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                include: /src/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+      publicPath: './',
+      path: path.resolve(__dirname, 'dist/'),
+      filename: 'js/app.bundle.js'
+  },
+  devServer: {
+      contentBase: './dist',
+      writeToDisk: true,
+      watchContentBase: true,
+      port: 9876
+  },
+  module: {
+      rules: [
+          {
+              test: /\.scss$/,
+              include: /src/,
+              use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'sass-loader',
 
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.html$/,
-                use: [
-                {
-                    loader: "html-loader",
-                    options: { minimize: false }
-                }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'assets/fonts/'
-                    }
-                  }
-                ]
-              },
+              ],
+          },
+          {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader']
+          },
+          {
+              test: /\.html$/,
+              use: [
               {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: './assets/images/'
-                    }
-                  }
-                ]
+                  loader: "html-loader",
+                  options: { minimize: false }
               }
+              ]
+          },
+          {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/fonts/'
+                  }
+                }
+              ]
+            },
+            {
+              test: /\.(png|svg|jpg|gif)$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: './assets/images/'
+                  }
+                }
+              ]
+            },
+            {
+              test: /\.mp4$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: './assets/videos/'
+                  }
+                }
+              ]
+            },
         ]
     },
     plugins: [
